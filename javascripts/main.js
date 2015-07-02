036bfef8bb337fa1ac3b732e3278ffbe71815646
Matt Schoen <mtschoen@gmail.com>
2015-07-02 00:41:52 -0400

    Looking pretty good. Seems like this might actually work.

--- javascripts/main.js
+++ javascripts/main.js
@@ -10,13 +10,15 @@ var moztrack = moztrack || {};
 	var db;
 	var progress;
 	
-	var startDate = new Date(2015, 6, 27, 12);
-	var endDate = new Date(2015, 8, 6, 19, 17);
+	var startDate = new Date(2015, 5, 27, 12);
+	var endDate = new Date(2015, 7, 6, 19, 17);
 	
-	var episodeNum = 2000;
+	var episodeNum = 2659;
 	
-	var showStartDate = new Date(1999, 1, 11);
-	var showEndDate = new Date(2015, 8, 6);
+	var showStartDate = new Date(1999, 0, 11);
+	var showEndDate = new Date(2015, 7, 6);
+	
+	var showTime, realTime;
 	
 	/**
 	 * json lists of episodes (by year) Data copied from Wikipedia, and converted with
@@ -33,19 +35,21 @@ var moztrack = moztrack || {};
 		console.log("Stream started at ", startDate);
 		console.log("Stream ends at ", endDate);
 		console.log("Stream duration: ", (endDate - startDate) / (60 * 60 * 1000), " hours");
+		var perEpisode = (endDate - startDate) / episodeNum;
+		console.log((perEpisode / (60 * 1000)) +  " minutes per episode");
 		
 		//Create tracker
 		var content = document.getElementById("moztrack-content");
 		progress = Div();
 		
-		var showTime = Input({},[],{"onkeyup":function(){
+		showTime = Input({},[],{"onkeyup":function(){
 			var date = new Date(this.value);
 			moztrack.getRealTime(date, function(date){
 				realTime.value = date.format("Y-m-d H:i:s");
 			});
 		}});
 		
-		var realTime = Input({},[],{"onkeyup":function(){
+		realTime = Input({},[],{"onkeyup":function(){
 			var date = new Date(this.value);
 			moztrack.getShowTime(date, function(date){
 				showTime.value = date.format("Y-m-d H:i:s");
@@ -66,39 +70,45 @@ var moztrack = moztrack || {};
 		//If table exists, assume we've done this before
 		doIfTableNotExist(db, "episodes", function(){
 			db.transaction(function(tx){
-				tx.executeSql('CREATE TABLE IF NOT EXISTS "episodes" ("id" INTEGER PRIMARY KEY  AUTOINCREMENT  NOT NULL  UNIQUE , "guest" VARCHAR, "date" DATETIME NOT NULL );', [],
+				tx.executeSql('CREATE TABLE IF NOT EXISTS "episodes" ("id" INTEGER PRIMARY KEY  AUTOINCREMENT  NOT NULL  UNIQUE , "guest" VARCHAR, "date" DATETIME NOT NULL, "realtime" DATETIME NOT NULL );', [],
 					function(tx, results){
 						//TODO: non-blocking solution
-						for(var i in episodeLists){
-							$.ajax({
-								//Use absolute URL for local testing, Chrome blocks ajax requests to file:///
-								"url":"http://mtschoen.github.io/month-of-zen-tracker/episodes/" + 1999 + ".json",
-								//"url":"episodes/" + episodeLists[i] + ".json",
-								"crossDomain":true,
-								"async":false,
-								"success":function(data){
-									var query = "INSERT INTO episodes (guest,date) VALUES (?,?)"
-									for(var j in data){
-										data[j].date = new Date(data[j].date);
-										data[j].date.setFullYear(episodeLists[i]);
-										
-										console.log(data[j].date.format("Y-m-d"));
-										progress.innerHTML = j;
-										tx.executeSql(query, [data[j].guest, data[j].date.format("Y-m-d")],
-											function(tx, results){
-												
-											},
-											function(tx, error){
-												console.log(error);
-												console.log(query);
-												console.log(data[j].guest, data[j].date.format("Y-m-d"));
-												throw error;
-											}
-										);
-									}
+						$.ajax({
+							//Use absolute URL for local testing, Chrome blocks ajax requests to file:///
+							"url":"http://mtschoen.github.io/month-of-zen-tracker/episodes/all.json",
+							//"url":"http://mtschoen.github.io/month-of-zen-tracker/episodes/" + 1999 + ".json",
+							//"url":"episodes/" + episodeLists[i] + ".json",
+							"crossDomain":true,
+							"success":function(data){
+								episodeNum = data.length - 1;	//-1 for sept11
+								var perEpisode = (endDate - startDate) / episodeNum;
+								var streamTime = 0;
+								var query = "INSERT INTO episodes (guest,date,realtime) VALUES (?,?,?)";
+								episodeNum = 0;
+								for(var j in data){
+									if(data[j].extra == "sept11")
+										continue;
+									episodeNum++;
+									data[j].date = new Date(data[j].date);
+									
+									progress.innerHTML = j;
+									
+									var realtime = new Date(startDate.getTime() + streamTime);
+									tx.executeSql(query, [data[j].guest, data[j].date.format("Y-m-d"), realtime],
+										function(tx, results){
+											
+										},
+										function(tx, error){
+											console.log(error);
+											console.log(query);
+											console.log(data[j].guest, data[j].date.format("Y-m-d"));
+											throw error;
+										}
+									);
+									streamTime += perEpisode;
 								}
-							});
-						}
+							}
+						});
 					},
 					function(tx, error){
 						console.log(error);
@@ -137,18 +147,24 @@ var moztrack = moztrack || {};
 	
 	moztrack.getRealTime = function(date){
 		if(date < showStartDate || date > showEndDate){
-			console.log("Invalid show date");
+			console.log("Invalid show date: ", date, showStartDate);
 			return;
 		}
 		db.transaction(function(tx){
-			tx.executeSQL("SELECT * FROM epixodes WHERE date = ?", [date.format("Y-m-d")],
+			tx.executeSql("SELECT * FROM episodes WHERE date = ?", [date.format("Y-m-d")],
 			function(tx, results){
 				console.log(results);
+				if(results.rows.length > 0){
+					//realTime.value = new Date(startDate.getTime() + results.rows[0].startms).format("Y-m-d H:i:s");
+					realTime.value = results.rows[0].realtime;
+				} else {
+					console.log("no results?");
+				}
 			},
 			function(tx, error){
 				console.log(error);
 				throw error;
-			})
+			});
 		});
 	};
 	moztrack.getShowTime = function(date){
@@ -156,5 +172,20 @@ var moztrack = moztrack || {};
 			console.log("Invalid real date");
 			return;
 		}
+		db.transaction(function(tx){
+			tx.executeSql("SELECT * FROM episodes WHERE date = ?", [date.format("Y-m-d")],
+			function(tx, results){
+				console.log(results);
+				if(results.rows.length > 0){
+					realTime.value = new Date(startDate.getTime() + results.rows[0].startms).format("Y-m-d H:i:s");
+				} else {
+					console.log("no results?");
+				}
+			},
+			function(tx, error){
+				console.log(error);
+				throw error;
+			});
+		})
 	};
 })();
