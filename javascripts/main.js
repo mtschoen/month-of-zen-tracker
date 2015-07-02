777c769b9fafa6ae07ad9c9ef134db5b8972135a
Matt Schoen <mtschoen@gmail.com>
2015-07-02 03:26:58 -0400

    Tweaked the thing again

--- javascripts/main.js
+++ javascripts/main.js
@@ -21,7 +21,7 @@ var moztrack = moztrack || {};
 	
 	var perEpisode = 28;
 	var perEpisodeOverride = 24;
-	moztrack.startOverride = new Date(2015,5,26,11,25);
+	moztrack.startOverride = new Date(2015,5,26,11,24);
 	
 	$(document).ready(function() {
 		//Setup local DB
