dc3c4655809ab0ad724455d476f745a0599be9e6
Matt Schoen <mtschoen@gmail.com>
2015-07-02 13:54:06 -0400

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -21,7 +21,7 @@ var moztrack = moztrack || {};
 	
 	var perEpisode = 28;
 	var perEpisodeOverride = 24;
-	moztrack.startOverride = new Date(2015,5,26,6,12);
+	moztrack.startOverride = new Date(2015,5,26,5,02);
 	
 	$(document).ready(function() {
 		//Setup local DB
